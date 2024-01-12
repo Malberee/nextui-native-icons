413612765005534c3036ab5b41430cba35f56e3d
Malberee <82259650+Malberee@users.noreply.github.com>
2024-01-12 15:03:54 +0200

    chore: add circle props

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import Svg, { Circle } from 'react-native-svg';
-import type { SvgProps } from 'react-native-svg';
+import type { CircleProps, SvgProps } from 'react-native-svg';
 
 interface Props extends SvgProps {
   trackColor: string;
   fillerColor: string;
+  trackProps: CircleProps;
+  fillerProps: CircleProps;
 }
 
-const SvgCircularProgress = ({ trackColor, fillerColor, ...props }: Props) => (
+const SvgCircularProgress = ({
+  trackColor,
+  fillerColor,
+  trackProps,
+  fillerProps,
+  ...props
+}: Props) => (
   <Svg fill="none" strokeWidth={3} viewBox="0 0 32 32" {...props}>
-    <Circle cx={16} cy={16} r={13} stroke={trackColor} />
+    <Circle cx={16} cy={16} r={13} stroke={trackColor} {...trackProps} />
     <Circle
       cx={16}
       cy={16}
@@ -18,6 +26,7 @@ const SvgCircularProgress = ({ trackColor, fillerColor, ...props }: Props) => (
       strokeDashoffset={61}
       stroke={fillerColor}
       strokeLinecap="round"
+      {...fillerProps}
     />
   </Svg>
 );
